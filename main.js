093184b7e0f771fd56a86e4645bdfa6ebff5ebdb
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-08-08 12:58:34 -0400

    final

---
+++ main.js
@@ -0,0 +1,24 @@
+let firstName = Joseph;
+let lastName = "Svoboda";
+var age = 27;
+
+firstName = "Joseph";
+lastName = Svoboda;
+age = 27;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentge = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
